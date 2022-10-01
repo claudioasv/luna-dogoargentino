827484ccecdb2b9449862805e4b12976421937d6
claudioas <claudioasepulvedav@gmail.com>
2022-10-01 01:06:55 -0300

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,17 @@
-// const API = 'https://instagram28.p.rapidapi.com/medias?user_id=48611850082&batch_size=9';
+// const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC_ntDY8t8158ynwX-YVPfhA&part=snippet%2Cid&order=date&maxResults=9';
+// const API = 'https://dog.ceo/api/breeds/image/random';
+// const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://photoslibrary.googleapis.com/v1/albums/AMQ41fHxrlTIRgy2GnTefhSiWHOSl3wHs_KetUQqpUMHGiPzv0n1-SWQOfQhMPTOYkZ1jeqZTjiR';
 const content = null || document.getElementById("content");
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4603ef4a49mshe6bad1e8d8a1dcep1ae0cejsna25e36e2436e',
-		'X-RapidAPI-Host': 'instagram28.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer oauth2-token',
+        // 'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
+    }
 };
 
 async function fethData(urlApi) {
@@ -14,9 +20,9 @@ async function fethData(urlApi) {
     return data;
 }
 
-// funcion que se llama a sí misma.
-(async () => {
+async function loadApi() {
     try {
+        console.log('funcion asíncrona');
         const videos = await fethData(API);
         let view = `
         ${videos.items.map(video => `
@@ -32,11 +38,37 @@ async function fethData(urlApi) {
                 </h3>
             </div>
         </div>
-        `).slice(0,4).join('')}        
+        `).slice(0, 4).join('')}        
         `;
         content.innerHTML = view;
     } catch (error) {
-        // alert('Hubo un error al cargar la API');
         console.log(error);
     }
-})();
+}
+
+// funcion que se llama a sí misma.
+// (async () => {
+    // console.log('inicio automático');
+//     try {
+//         const videos = await fethData(API);
+//         let view = `
+//         ${videos.items.map(video => `
+//         <div class="group relative">
+//             <div
+//                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+//                 <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+//             </div>
+//             <div class="mt-4 flex justify-between">
+//                 <h3 class="text-sm text-gray-700">
+//                     <span aria-hidden="true" class="absolute inset-0"></span>
+//                     ${video.snippet.title}
+//                 </h3>
+//             </div>
+//         </div>
+//         `).slice(0, 4).join('')}
+//         `;
+//         content.innerHTML = view;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
